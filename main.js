f96a29e2e313e4a4eb828069fc119e04d75b179c
root <root@localhost.localdomain>
2019-04-16 18:17:51 -0500

    Create project

---
+++ main.js
@@ -0,0 +1,41 @@
+const indexCollection = function(db, callback) {
+  db.collection('documents').createIndex(
+    { "a": 1 },
+      null,
+      function(err, results) {
+        console.log(results);
+        callback();
+    }
+  );
+};
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Create a new MongoClient
+const client = new MongoClient(url,{ useNewUrlParser: true });
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+   indexCollection(db, function() {
+      client.close();
+    });
+
+  //insertDocuments(db, function() {
+  //  client.close();
+  //});
+
+
+  //client.close();
+});
