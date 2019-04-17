4ce6784f35914ad1f62d6e910b12c13f852eec88
ytljc2003 <ytljc2003@gmail.com>
2019-04-17 17:57:44 -0500

    Fix readdb interface from arguments to using callback

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-var readdb = require('./readdb.js');
-
-var rdb = new readdb.Readdb();;
+var rdb = require('./readdb.js');
 
 app.get('/index.html', function(req, res){
 	res.writeHead(200, {'Content-Type':'text/html'});
 	res.write('<head><meta charset="utf-8"></head>');
 
 	res.write('<p>Here run in Express!</p>');
-	//res.write(readdb.readdb());
-	console.log(rdb());
+	
+	rdb.Readdb( function(result){
+			res.write(JSON.stringify(result));
+			//console.log(JSON.stringify(result));
+		}
+	);
 });
+
 
 app.listen(3000,"192.168.208.139");
--- readdb.js
+++ readdb.js
@@ -1,20 +1,20 @@
-findDocuments = function(db, callback) {
+const assert = require('assert');
+
+const findDocuments = function(db, callback) {
   // Get the documents collection
   const collection = db.collection('documents');
 
   // Find some documents
-  collection.find({a:1},field:{_id: 1, 'a': 0}).toArray(function(err, docs) {
-    //console.log(err);
-    //assert.equal(null,err);
+  collection.find({}/*,{fields:{_id:0 , a: 1}}*/).project({_id:0}).toArray(function(err, docs) {
+    assert.equal(null,err);
     //console.log("Found the following records");
     //console.log(docs)
     callback(docs);
   });
 }
 
-readdb = function(){
+readdb = function(cb){
 	const MongoClient = require('mongodb').MongoClient;
-	const assert = require('assert');
 
 	// Connection URL
 	const url = 'mongodb://localhost:27017';
@@ -34,20 +34,13 @@ readdb = function(){
 
    		findDocuments(db, function(result) {
 			//console.log(result);
+			cb(result);
       			client.close();
-			return result;
     		});
 
 	});
 }
 
-exports.Readdb = function(){
-	var result;
-
-	return function()
-	{
-		return readdb();
-	}
-}
+exports.Readdb = readdb;
 
 //readdb();
